26cfa45fdbfe360483b5f32f6609dad4c0ca20ef
Dacy Lin <129496061+dacylin@users.noreply.github.com>
2025-01-11 12:44:13 +0800

    update_0111 重構

--- demoFront/frontend/src/views/UnauthorizedView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-      <h1>Unauthorized</h1>
-      <p>You do not have permission to access this page.</p>
-      <router-link to="/">Go to Home</router-link>
-    </div>
-  </template>
-  
